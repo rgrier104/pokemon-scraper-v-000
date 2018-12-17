cf6dbf7cf7e3e8e13789a0128ec9c916da4a8462
Rachel Grier <rgrier104@gmail.com>
2018-12-17 00:22:00 +0000

    Automatically backed up by Learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -40,23 +40,23 @@ describe "Pokemon" do
     end
   end
 
-  describe "BONUS" do
-
-    before do
-      @sql_runner.execute_create_hp_column
-      Pokemon.save('Pikachu', 'electric', @db)
-      Pokemon.save('Magikarp', 'water', @db)
-    end
-
-    let(:pikachu){Pokemon.find(1, @db)}
-    let(:magikarp){Pokemon.find(2, @db)}
-
-    # remove the 'x' before 'it' to run these tests
-    it "knows that a pokemon have a default hp of 60" do
-      # The find method should create a new Pokemon object with an id, type, name AND hp after selecting their row from the database by their id number.
-      # remember to also update the initialize method to accept an argument of hp that defaults to nil if not set (so it still passes the non-bonus tests)
-      expect(@db.execute("SELECT hp FROM pokemon").flatten.first).to eq(60)
-    end
+  # describe "BONUS" do
+  #
+  #   before do
+  #     @sql_runner.execute_create_hp_column
+  #     Pokemon.save('Pikachu', 'electric', @db)
+  #     Pokemon.save('Magikarp', 'water', @db)
+  #   end
+  #
+  #   let(:pikachu){Pokemon.find(1, @db)}
+  #   let(:magikarp){Pokemon.find(2, @db)}
+  #
+  #   # remove the 'x' before 'it' to run these tests
+  #   it "knows that a pokemon have a default hp of 60" do
+  #     # The find method should create a new Pokemon object with an id, type, name AND hp after selecting their row from the database by their id number.
+  #     # remember to also update the initialize method to accept an argument of hp that defaults to nil if not set (so it still passes the non-bonus tests)
+  #     expect(@db.execute("SELECT hp FROM pokemon").flatten.first).to eq(60)
+  #   end
 
   #   # So Ian and you have decided to battle.  He chose Magikarp (rookie mistake), and you chose Pikachu.
   #   # He used splash. It wasn't very effective. It did one damage.
